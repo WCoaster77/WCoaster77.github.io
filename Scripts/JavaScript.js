1f1bc7b0a52bb2db89e2dc0e9e304c6f9be69f9a
Data Detective <datadetectiveagent007@gmail.com>
2016-09-18 22:30:01 -0700

    Initial Commit
    My Personal Web Page

---
+++ Scripts/JavaScript.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function() {
+    setBindings();
+});
+
+
+//Allow screen to Scroll to selected section
+function setBindings() {
+    $("nav a").click(function (tip){
+        tip.preventDefault();
+        var sectionID = "#" + tip.currentTarget.id + "Sect";
+        //alert('button id ' + sectionID);
+        
+        $("html body").animate({
+            scrollTop: $(sectionID).offset().top
+        }, 1000)
+    })
+}
---
+++ Scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="bootstrap.js" />
+/// <reference path="JavaScript.js" />
+/// <reference path="jquery-3.1.0.js" />
+/// <reference path="jquery-3.1.0.slim.js" />
