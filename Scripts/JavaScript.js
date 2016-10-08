06fe10ce245a281879e76157f1c0a0bb87112f0b
Data Detective <datadetectiveagent007@gmail.com>
2016-10-08 09:00:31 -0700

    Latest Update to Promo

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -5,13 +5,13 @@
 
 //Allow screen to Scroll to selected section
 function setBindings() {
-    $("nav a").click(function (tip){
+    $("nav a").click(function (tip) {
         tip.preventDefault();
         var sectionID = "#" + tip.currentTarget.id + "Sect";
         //alert('button id ' + sectionID);
-        
+
         $("html body").animate({
             scrollTop: $(sectionID).offset().top
-        }, 1000)
-    })
+        }, 1000);
+    });
 }
