36d3a57849040c87c2af1f2de66fba3d2da7b7b7
Data Detective <datadetectiveagent007@gmail.com>
2016-12-19 23:45:13 -0800

    Update to match Gits

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -5,7 +5,7 @@
 
 //Allow screen to Scroll to selected section
 function setBindings() {
-    $("nav a").click(function (tip) {
+    $("nav ul li a").click(function (tip) {
         tip.preventDefault();
         var sectionID = "#" + tip.currentTarget.id + "Sect";
         //alert('button id ' + sectionID);
